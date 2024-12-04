0a6478b178dfc49dd3470044f306cdb6d4b81c69
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2024-12-04 17:30:38 +0530

    Delete week5-offline directory

--- week5-offline/assignment/assignment1.js
+++
@@ -1,3 +0,0 @@
-// Assignment #1 - Create a map functions that takes 2 inputs an array and a transformation callback/function
-// and transform the array into a new one using transformation function
-
--- week5-offline/assignment/assignment2.js
+++
@@ -1,3 +0,0 @@
-// Assignment #2 - Create a map functions that takes an array and 
-// a transform function as input and returns the transformed array as output
-
