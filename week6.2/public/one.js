6ecf52dd3deac75f058a0693c14588bdd23292cf
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2024-12-04 17:32:20 +0530

    Delete week6.2 directory

--- week6.2/public/one.js
+++
@@ -1,9 +0,0 @@
-async function signup(){
-    const username = document.getElementById("signup-username").value;
-    const password = document.getElementById("signup-password").value;
-     await axios.post("http://localhost:3000/signup",{
-        username:username,
-        password:password
-     });
-     alert("You are signed in");
-};
