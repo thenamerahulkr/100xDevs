c773c233c8e8dc55b9b00f369cf2eef211602eb8
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2024-12-04 17:30:29 +0530

    Delete week4 directory

--- week4/index.js
+++
@@ -1,9 +0,0 @@
-const { Command } = require('commander');
-const program = new Command();
-program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
-program.command('count_words')
-    .description('')
-    .argument(  )
--- week4/index.mjs
+++
@@ -1,4 +0,0 @@
-import chalk from 'chalk';
-console.log(chalk.bgBlue("rahul"));
-console.log(chalk.green.bold("hi there"));
-console.log(chalk.red.underline("hello ji kaise ho"));
